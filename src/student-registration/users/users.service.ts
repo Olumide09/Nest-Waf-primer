a7e121c1f6be851046e9d59f9a6fc522dbb4a4d9
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-02-20 18:31:26 +0100

    Update users.service.ts

--- src/student-registration/users/users.service.ts
+++ src/student-registration/users/users.service.ts
@@ -6,23 +6,32 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectRepository(User)
-    private usersRepository: Repository<User>
-  ) { }
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+constructor(
+//inject user repository for use here in UsersService as if it is part of the class
+@InjectRepository(User)
+private usersRepository: Repository<User>
+){}
+async create(createUserDto: CreateUserDto) {
+  const newUser: User = this.usersRepository.create(createUserDto)
+  return this.usersRepository.save(newUser);
+  //return 'This action adds a new user';
   }
-  findAll() {
-    return `This action returns all users`;
-  }
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-  remove(id: number) {
-    return `This action removes a #${id} user`
-  }
-}
+  async findAll() {
+    //return `This action returns all users`;
+    return await this.usersRepository.find();
+    }
+    async findOne(id: number) {
+      //return `This action returns a #${id} user`;
+      return await this.usersRepository.findOne(id);
+      }
+      async update(id: number, updateUserDto:
+        UpdateUserDto) {
+        //return `This action updates a #${id} user`;
+        return await this.usersRepository.update(id,
+        updateUserDto);
+        }
+        async remove(id: number) {
+          //return `This action removes a #${id} user`;
+          return await this.usersRepository.delete(id);
+          }
+          }
